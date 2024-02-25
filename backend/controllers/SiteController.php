1fa4ddd86c1eb1388601ef84ae00f8df432a6f9c
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2024-02-25 21:20:25 +0500

    sdf

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -2,11 +2,15 @@
 
 namespace backend\controllers;
 
-use common\models\LoginForm;
+use backend\models\LoginForm;
+use backend\models\User;
 use Yii;
+use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 use yii\web\Controller;
+use yii\web\HttpException;
 use yii\web\Response;
 
 /**
@@ -20,27 +24,17 @@ class SiteController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::class,
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['logout', 'index'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
+            'corsFilter'=>[
+                'class' => Cors::class,
             ],
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'logout' => ['post'],
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::class,
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
                 ],
             ],
         ];
+
     }
 
     /**
@@ -55,50 +49,78 @@ class SiteController extends Controller
         ];
     }
 
-    /**
-     * Displays homepage.
-     *
-     * @return string
-     */
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
-
-    /**
-     * Login action.
-     *
-     * @return string|Response
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $this->layout = 'blank';
+    public function actionLogin(){
 
         $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
+
+        if ($model->load(Yii::$app->request->getBodyParams(),'')) {
+
+            if( $model->login()){
+
+                $user = Yii::$app->user->identity;
+
+                $token = $this->generateToken($user);
+
+
+                return [
+                    'token' => (string) $token,
+                ];
+            }else{
+                return [$model->getFirstErrors()];
+            }
+        } else {
+            throw new HttpException(400, 'Bad request');
         }
-
-        $model->password = '';
-
-        return $this->render('login', [
-            'model' => $model,
-        ]);
     }
 
-    /**
-     * Logout action.
-     *
-     * @return Response
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
 
-        return $this->goHome();
+    public function actionRefreshToken(){
+        if($token = Yii::$app->request->post('token')){
+
+            $user = User::findOne(['access_token'=>$token]);
+
+            $token = $this->generateToken($user);
+
+            return [
+                'token' => (string) $token,
+            ];
+        }else{
+            throw new HttpException(204, 'OK');
+            return false;
+        }
+    }
+
+    private function generateToken($user)
+    {
+        $expiry = 86400;
+        $issuedAt = time();
+        $expire = $issuedAt + $expiry;
+        $payload = [
+            'id' => $user->id,
+            'iat' => $issuedAt,
+            'nbf' => $expire,
+            'name'=>$user->name,
+        ];
+        $token = Yii::$app->jwt->generateToken($payload);
+
+
+        return $token;
+    }
+
+    public function actionValidateToken($token)
+    {
+        $isValid = Yii::$app->jwt->validateToken($token);
+        return ['valid' => $isValid];
+    }
+
+
+    public function actionCheck(){
+        $token = Yii::$app->user->identity->access_token;
+
+        $decode = Yii::$app->jwt->decodeToken($token);
+
+        return ['nbf'=>$decode->nbf,'time'=>time(),
+            'check'=>$decode->nbf < time()];
+
     }
 }
---
+++ backend/controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace backend\controllers;
+
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\ContentNegotiator;
+use yii\filters\Cors;
+use yii\rest\Controller;
+use yii\web\Response;
+
+
+class UserController extends Controller
+{
+    public function behaviors() {
+
+        return [
+            'corsFilter'=>[
+                'class' => Cors::class,
+            ],
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::class,
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                ],
+            ],
+            'authenticator' => [
+                'class' => CompositeAuth::class,
+                'authMethods' => [
+                    HttpBearerAuth::class,
+                ],
+            ]
+        ];
+    }
+
+    public function actionMe()
+    {
+
+        return Yii::$app->user->identity->name;
+    }
+
+
+
+}
