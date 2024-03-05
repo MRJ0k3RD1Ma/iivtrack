d419b054026481412fcf17e21b1f0de92eb5b9d8
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2024-03-05 15:52:11 +0500

    sdf

--- frontend/modules/cc/views/default/nonactive.php
+++ frontend/modules/cc/views/default/nonactive.php
@@ -2,26 +2,30 @@
 /* @var $model \common\models\User[]*/
 $this->title = "Hozrda offline bo`lgan hodimlar";
 ?>
-<div class="table-responsive">
-<table class="table table-bordered table-hover">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>FIO</th>
-            <th>Tel</th>
-            <th>So'ngi aktivlik</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($model as $key=>$item): ?>
-        <tr>
-            <td><?= $key+1?></td>
-            <td><?= $item->name ?></td>
-            <td><?= $item->username ?></td>
-            <td><?= $item->active_date ?></td>
-        </tr>
-    <?php endforeach;?>
-    </tbody>
-</table>
+<div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>FIO</th>
+                    <th>Tel</th>
+                    <th>So'ngi aktivlik</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($model as $key=>$item): ?>
+                    <tr>
+                        <td><?= $key+1?></td>
+                        <td><?= $item->name ?></td>
+                        <td><?= $item->username ?></td>
+                        <td><?= $item->active_date ?></td>
+                    </tr>
+                <?php endforeach;?>
+                </tbody>
+            </table>
 
+        </div>
+    </div>
 </div>
