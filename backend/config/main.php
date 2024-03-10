8631c85d7c0bd964ffaf870105d21c02702d2961
Dilmurod <mr.j0k3r.d1ma@gmail.com>
2024-03-10 21:22:04 +0500

    sdf

--- backend/config/main.php
+++ backend/config/main.php
@@ -63,6 +63,7 @@ return [
                     'extraPatterns'=>[
                         'GET me'=>'me',
                         'GET track'=>'track',
+                        'GET view'=>'view',
                     ]
                 ],
             ],
